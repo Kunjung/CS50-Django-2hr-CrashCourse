6df10b574bb845083ea000c0adee07ea29a28f0f
Kunjung <kunjungsherpa@gmail.com>
2019-04-06 22:06:05 +0545

    First

---
+++ hello/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.urls import reverse
+
+from .models import Flight, Passenger	
+# Create your views here.
+
+def index(request):
+	context = {
+		"flights": Flight.objects.all()
+	}
+	return render(request, "hello/index.html", context)
+
+def flight(request, flight_id):
+	try:
+		flight = Flight.objects.get(pk=flight_id)
+	except Flight.DoesNotExist:
+		raise Http404("Flight does not exist.")
+	context = {
+		"flight": flight,
+		"passengers": flight.passengers.all(),
+		"non_passengers": Passenger.objects.exclude(flights=flight).all()
+	}
+	return render(request, "hello/flight.html", context)
+
+
+def book(request, flight_id):
+	try:
+		passenger_id = int(request.POST["passenger"])
+		passenger = Passenger.objects.get(pk=passenger_id)
+		flight = Flight.objects.get(pk=flight_id)
+	except KeyError:
+		return render(request, "hello/error.html", {"message": "No selection."})
+	except Passenger.DoesNotExist:
+		return render(request, "hello/error.html", {"message": "No passenger."})
+	except Flight.DoesNotExist:
+		return render(request, "hello/error.html", {"message": "No Flight."})
+	except:
+		return render(request, "hello/error.html", {"message": "Unknown Error"})
+
+	passenger.flights.add(flight)
+	return HttpResponseRedirect(reverse("flight", args=(flight_id,) ))
